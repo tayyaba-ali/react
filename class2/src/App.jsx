1f57c8ea4ed2748a12c5f3172df8f0b254e29223
tayyaba-ali <ta6402792@gmail.com>
2024-12-13 17:58:02 +0500

    components

---
+++ class2/src/App.jsx
@@ -0,0 +1,17 @@
+// import "./App.css";
+import Navbar from "./components/Navbar";
+let firstName = "Tayyaba";
+function App() {
+  return (
+    <div>
+      <Navbar />
+
+      <h1 className="heading">hello {firstName}</h1>
+      <img src="" alt="" />
+      {/* <input type="text" htmlFor="text" /> */}
+      {/* <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt, voluptas.</p> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ class2/src/components/hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import Navbar from './Navbar.jsx'
+function hero() {
+  return (
+   <Navbar />
+  )
+}
+
+export default hero
